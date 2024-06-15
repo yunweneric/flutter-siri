ab0496f773049dda4b2b1d5cd003265b3ef4cd8d
yunweneric <yunweneric@gmail.com>
2024-06-15 13:31:40 +0100

    Cloned UI from flutter-open-uis

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_openui/screens/splash_screen.dart';
+import 'package:flutter_openui/utils/colors.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Bot',
+      theme: ThemeData(
+        primaryColorLight: AppColors.white,
+        primaryColorDark: AppColors.bgColor,
+        primaryColor: AppColors.primary,
+        cardColor: AppColors.bgCard,
+        scaffoldBackgroundColor: AppColors.bgColor,
+        textTheme: TextTheme(
+          displayLarge: GoogleFonts.inter(fontSize: 35, color: AppColors.white, fontWeight: FontWeight.w600),
+          displayMedium: GoogleFonts.inter(fontSize: 20, color: AppColors.white, fontWeight: FontWeight.w500),
+          displaySmall: GoogleFonts.inter(fontSize: 14, color: AppColors.white, fontWeight: FontWeight.w500),
+          bodyMedium: GoogleFonts.inter(fontSize: 14, color: AppColors.white, fontWeight: FontWeight.w500),
+        ),
+        chipTheme: ChipThemeData(
+          backgroundColor: AppColors.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(18),
+            side: const BorderSide(color: AppColors.primary),
+          ),
+        ),
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import path_provider_foundation
+import speech_to_text_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SpeechToTextMacosPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextMacosPlugin"))
+}
